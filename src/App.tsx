4c5075c9999f7162260c6ecda945dac4597efc9c
Bahtiyar89 <bahtiyar1989@gmail.com>
2023-03-09 00:00:53 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { LeafPoll, Result } from "react-leaf-polls";
+import "react-leaf-polls/dist/index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Persistent data array (typically fetched from the server)
+const resData = [
+  { id: 0, text: "Answer 1", votes: 0 },
+  { id: 1, text: "Answer 2", votes: 0 },
+  { id: 2, text: "Answer 3", votes: 0 },
+];
+
+// Object keys may vary on the poll type (see the 'Theme options' table below)
+const customTheme = {
+  textColor: "black",
+  mainColor: "#00B87B",
+  backgroundColor: "rgb(255,255,255)",
+  alignment: "center",
+};
+
+function vote(item: Result, results: Result[]) {
+  // Here you probably want to manage
+  // and return the modified data to the server.
+  console.log(item);
+  console.log(results);
 }
+
+const App = () => {
+  return (
+    <>
+      <LeafPoll
+        type="multiple"
+        question="What you wanna ask?"
+        results={resData}
+        theme={customTheme}
+        onVote={vote}
+        isVoted={false}
+      />
+      <LeafPoll
+        type="multiple"
+        question="What you wanna ask?"
+        results={resData}
+        theme={customTheme}
+        onVote={vote}
+        isVoted={false}
+      />
+      <LeafPoll
+        type="multiple"
+        question="What you wanna ask?"
+        results={resData}
+        theme={customTheme}
+        onVote={vote}
+        isVoted={false}
+      />
+    </>
+  );
+};
 
 export default App;
